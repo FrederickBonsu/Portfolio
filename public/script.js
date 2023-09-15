8d4a7564f072e0c2d6ffc19b8aab4378f6abae62
FrederickBonsu <FrederickBonsu36060@gmail.com>
2023-09-15 15:51:50 +0000

    moved the index.js file to the main project  directory

--- public/script.js
+++ public/script.js
@@ -1,6 +1,19 @@
 const hamburger = document.querySelector("#hamburger");
+const moon = document.querySelector(".moon");
 const menu = document.querySelector("#menu");
+const hLinks = document.querySelectorAll("#hLink");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   hamburger.classList.toggle("bg-white");
+});
+
+hLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("Hidden");
+    hamburger.classList.toggle("bg-white");
+  });
+});
+
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
 });
